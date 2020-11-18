f241f4431f48c48b4084c54b974ca48b58f4cdae
Denis <daen252@gmail.com>
2020-11-18 20:35:03 +0300

    Update Procfile_

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -13,14 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '118n@88fuxo=su2152yqo@+r$90af654)j)&my*f8d@defix1y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
